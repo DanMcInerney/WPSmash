7c87c1e23d5f164374738bfece40c14288876f8b
Dan McInerney <danhmcierney@gmail.com>
2015-11-24 00:09:04 -0700

    everything set until user makes target choice

--- WPSmash.py
+++ WPSmash.py
@@ -2,15 +2,15 @@
 
 import io
 import os
-from subprocess import Popen, PIPE, STDOUT
-import argparse
+import re
+import sys
+import time
+import fcntl
 import socket
 import random
 import struct
-import fcntl
-import re
-import time
-import sys
+import argparse
+from subprocess import Popen, PIPE, STDOUT
 
 DN = open(os.devnull, 'w')
 # Console colors
@@ -57,7 +57,6 @@ def get_mon_iface(args):
     Get the monitor mode interface name
     '''
     # Kill any potentialy interfering programs
-    Popen('airmon-ng check kill'.split(), stdout=DN, stderr=DN)
     monitors, wifi_intfs = iwconfig()
 
     if args.interface:
@@ -69,26 +68,18 @@ def get_mon_iface(args):
         else:
             iface = get_iface(wifi_intfs)
 
-    print iface
     orig_mac = get_mac(iface)
-    print orig_mac
+    print '[*] Original MAC address: %s' % orig_mac
     #Changes MAC and brings it up in monitor mode
-    rand_mac(iface)
+    new_mac = rand_mac(iface)
+    print '[*] New MAC address: %s' % new_mac
 
     return orig_mac, iface
 
-def start_mon_mode(interface):
-    '''
-    Starts monitor mode on the interface
-    '''
-    print '['+G+'+'+W+'] Starting monitor mode on '+G+interface+W
-    try:
-        Popen('airmon-ng check kill'.split(), stdout=DN, stderr=DN)
-        rand_mac(interface)
-        return interface
-    except Exception:
-        raise
-        sys.exit('['+R+'-'+W+'] Could not start monitor mode')
+def get_mac(interface):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', interface[:15]))
+    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
 
 def rand_mac(interface):
     '''
@@ -106,8 +97,12 @@ def rand_mac(interface):
     os.system('/sbin/iwconfig %s mode monitor' % interface)
     os.system('/sbin/ip link set %s up' % interface)
 
-def get_iface(wifi_intfs):
+    return mac
 
+def get_iface(wifi_intfs):
+    '''
+    Get the interface we're going to interact with
+    '''
     if len(wifi_intfs) < 1:
         sys.exit('['+R+'-'+W+'] No wireless interfaces found, bring one up and try again')
 
@@ -123,56 +118,90 @@ def get_best_intf(wifi_intfs):
     scanned_aps = []
 
     for iface in wifi_intfs:
-        count = 0
-        proc = Popen(['/sbin/iwlist', iface, 'scan'], stdout=PIPE, stderr=DN)
-        for line in proc.communicate()[0].split('\n'):
-            if ' - Address:' in line: # first line in iwlist scan for a new AP
-               count += 1
+        count = iface_scan(iface)
+        if count == 0:
+            print '['+G+'+'+W+'] Networks discovered by '+G+iface+W+': '+T+str(count)+W
+            print '['+R+'-'+W+'] Rescanning '+G+iface+W
+            count = iface_scan(iface)
         scanned_aps.append((count, iface))
         print '['+G+'+'+W+'] Networks discovered by '+G+iface+W+': '+T+str(count)+W
 
     # Returns the interface that found the most APs
     return max(scanned_aps)[1]
 
-def get_wash_output(mon_iface):
+def iface_scan(iface):
+    count = 0
+    proc = Popen(['/sbin/iwlist', iface, 'scan'], stdout=PIPE, stderr=DN)
+    for line in proc.communicate()[0].split('\n'):
+        if ' - Address:' in line: # first line in iwlist scan for a new AP
+           count += 1
+    return count
+
+def select_target(mon_iface):
     '''
     Get wash output
     '''
+    output = []
     filename = 'wash.log'
-    with io.open(filename, 'wb') as writer, io.open(filename, 'rb', 1) as reader:
-        cmd = 'wash -i {} -C'.format(mon_iface)
-        print '[*] Running `{}` for 10 seconds'.format(cmd)
-        proc = Popen(cmd.split(), stdout=writer)
-        time.sleep(5)
-        out = reader.readlines()
-        return out
+    try:
+        with io.open(filename, 'wb') as writer, io.open(filename, 'rb', 1) as reader:
+            cmd = 'wash -i {} -C'.format(mon_iface)
+            print '[*] Running `{}`'.format(cmd)
+            time.sleep(3)
+            proc = Popen(cmd.split(), stdout=writer)
+            # wash never stops
+            while proc.poll() is None:
+                output += reader.readlines()
+                targets = print_targets(output)
+                time.sleep(.25)
+    except KeyboardInterrupt:
+        choice = raw_input('[*] Enter the number of your choice: ')
+        return output, targets[choice]
+
+def print_targets(output):
+    '''
+    Print the targets and return them in a dict
+    '''
+    os.system('clear')
+    print '[*] Targets:\n'
+    targets = get_targets(output)
+    for idx in targets:
+        mac, chan, locked, essid = targets[idx]
+        print '[{}] {} {} {}'.format(idx, mac, essid, locked)
+    print '\n[*] Hit Ctrl-C to make a selection'
+
+    return targets
 
 def get_targets(out):
     '''
     Parse wash output for target macs and essids
     '''
-    targets = []
+    targets = {}
+    counter = 0
     for line in out:
         if line.count(':') == 5:
+            counter += 1
             line = line.split()
             # For some reason sometimes 1 AP around me throws tons of \x00's in
             # the mac
             mac = line[0].replace('\x00', '')
             chan = line[1]
+            locked = line[4]
             essid = line[5]
-            targets.append((essid, chan, mac))
-            print mac, chan, essid
+
+            if 'yes' in locked.lower():
+                locked = 'Locked'
+            else:
+                locked = ''
+
+            targets[str(counter)] = (mac, chan, locked, essid)
+
     return targets
 
 def run_reaver(targets, mon_iface):
     for t in targets:
         essid, chan, mac = t
         cmd = 'reaver -i {} -c {} -b {} -vv -S'.format(mon_iface, chan, mac)
-
-def get_mac(interface):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', interface[:15]))
-    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
 
 def cleanup(orig_mac, mon_iface):
     '''
@@ -190,13 +219,9 @@ def cleanup(orig_mac, mon_iface):
 def main():
     args = parse_args()
     orig_mac, mon_iface = get_mon_iface(args)
-    out = get_wash_output(mon_iface)
-    targets = get_targets(out)
-    try:
-        while 1:
-            print targets
-            time.sleep(1)
-    except KeyboardInterrupt:
-        cleanup(orig_mac, mon_iface)
+    out, target = select_target(mon_iface)
+    mac, chan, locked, essid = target
+    print 'You chose:', mac, chan, locked, essid
+    cleanup(orig_mac, mon_iface)
 
 main()
